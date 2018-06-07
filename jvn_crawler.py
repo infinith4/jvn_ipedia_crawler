4d5b4f7ae557fc295a052a57c50457ca041de2f5
tashiro.hiroshi <tashiro.hiroshi@toyoko-sys.co.jp>
2018-06-07 19:36:33 +0900

    jvn crawler

--- jvn_crawler.py
+++ jvn_crawler.py
@@ -1,25 +1,47 @@
 from utils import url_util
 from bs4 import BeautifulSoup 
 import urllib3
+import logging.config
+import yaml
 
-urlUtil = url_util.UrlUtil("")
-urlList = urlUtil.RequestUrl()
-http = urllib3.PoolManager()
-for url in urlList:
-    jvnUrl = "https://jvndb.jvn.jp" + url
-    res = http.request('GET', jvnUrl)
-    print(jvnUrl)
-    soup = BeautifulSoup(res.data, 'html.parser')
-    table = soup.findAll("table",{"class":"vuln_table_clase"})[0]
-    rows = table.findAll("tr")
-    rowIndex = 0
-    for i in rows:
-        td = i.findAll("td")
-        if td != None and len(td) != 0:
-            solution = td[0].select("a[name^='solution']")
-            if solution != None and len(solution) != 0:
-                if "対策" in solution[0].get_text():
-                    tr = rows[rowIndex + 1].get_text()
-                    if "ベンダより正式な対策が公開されています。" in tr:
-                        print("OK")
-        rowIndex = rowIndex + 1
+logging.config.fileConfig("logging_debug.conf")
+logger = logging.getLogger()
+
+try:
+    conf = open("jvn_crawler.yml", "r+")
+    confdata = yaml.load(conf)
+    # proxyUrl = ""
+    # jvnBaseUrl = ""
+    # jvnRequestUrl = ""
+    proxyUrl = confdata['PROXY_URL']
+    jvnBaseUrl = confdata['JVN_BASE_URL']
+    jvnRequestUrl = confdata['JVN_REQUEST_URL']
+
+    urlUtil = url_util.UrlUtil(jvnRequestUrl, proxyUrl)
+    urlList = urlUtil.RequestUrl()
+    http = urllib3.ProxyManager(proxy_url = proxyUrl, maxsize=10)
+    for url in urlList:
+        jvnUrl = jvnBaseUrl + url
+        logger.info(jvnUrl)
+        res = http.request('GET', jvnUrl)
+        soup = BeautifulSoup(res.data, 'html.parser')
+        table = soup.findAll("table",{"class":"vuln_table_clase"})[0]
+        rows = table.findAll("tr")
+        rowIndex = 0
+        for i in rows:
+            td = i.findAll("td")
+            if td != None and len(td) != 0:
+                solution = td[0].select("a[name^='solution']")
+                if solution != None and len(solution) != 0:
+                    if "対策" in solution[0].get_text():
+                        tr = rows[rowIndex + 1].get_text()
+                        logger.info("## 対策")
+                        if "ベンダより正式な対策が公開されています。" in tr:
+                            logger.info(tr)
+                            logger.info("OK")
+                        else:
+                            logger.info(tr)
+            rowIndex = rowIndex + 1
+except Exception as ex:
+    logger.error(ex)
+    print(ex)
--- utils/url_util.py
+++ utils/url_util.py
@@ -2,25 +2,33 @@ import urllib3
 from bs4 import BeautifulSoup 
 
 class UrlUtil:
-    def __init__(self, req_url):
-        self.req_url = req_url
+    def __init__(self, reqUrl, proxyUrl):
+        self.reqUrl = reqUrl
+        self.proxyUrl = proxyUrl
 
     def RequestUrl(self):
         try :
             urlList=[]
-            url = "https://jvndb.jvn.jp/search/index.php?mode=_vulnerability_search_IA_VulnSearch&lang=ja&keyword=python&useSynonym=1&vendor=&product=&datePublicFromYear=&datePublicFromMonth=&datePublicToYear=&datePublicToMonth=&dateLastPublishedFromYear=&dateLastPublishedFromMonth=&dateLastPublishedToYear=&dateLastPublishedToMonth=&cwe=&searchProductId="
-            http = urllib3.PoolManager()
-            res = http.request('GET', url)
-            soup = BeautifulSoup(res.data, 'html.parser')
-            #netPage = soup.findAll("a",{"title":"next page"})
+            url = self.reqUrl
 
-            table = soup.findAll("table",{"class":"result_class"})[0]
-            rows = table.findAll("tr")
-            for i in rows:
-                td = i.findAll("td")
-                if td != None and len(td) != 0:
-                    print(td[0].findAll('a')[0].get("href"))
-                    urlList.append(td[0].findAll('a')[0].get("href"))
+            http = urllib3.ProxyManager(proxy_url = self.proxyUrl, maxsize=10)
+            res = http.request('GET', url)
+            while True:
+                soup = BeautifulSoup(res.data, 'html.parser')
+                table = soup.findAll("table",{"class":"result_class"})[0]
+                rows = table.findAll("tr")
+                for i in rows:
+                    td = i.findAll("td")
+                    if td != None and len(td) != 0:
+                        print(td[0].findAll('a')[0].get("href"))
+                        urlList.append(td[0].findAll('a')[0].get("href"))
+                nextPage = soup.findAll("a",{"title":"next page"})
+                if len(nextPage) > 0:
+                    print(nextPage[0])
+                    print(nextPage[1])
+                    res = http.request('GET', "https://jvndb.jvn.jp/search/" + nextPage[0].get('href'))
+                else:
+                     break   
             return urlList
         except Exception as ex:
             print("アクセスに失敗しました。")
